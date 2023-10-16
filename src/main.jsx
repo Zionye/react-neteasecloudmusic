f83143fa103c2c3c93607f70e13c709f56deaf03
ruanm <247987314@qq.com>
2023-10-16 11:20:34 +0800

    fix 引入引入Ant Design 5.x, 并设置Antd为中文语言

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx' // 表示该文件当前路径下的App.jsx（相对路径）
 import App from '@/App' // 表示src/App.jsx，等价于上面的文件地址（绝对路径）
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/locale/zh_CN' // 引入Ant Design中文语言包
 import '@/common/styles/frame.styl' // 全局样式
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ConfigProvider locale={zhCN}>
     <App />
-  </React.StrictMode>,
+  </ConfigProvider>,
 )

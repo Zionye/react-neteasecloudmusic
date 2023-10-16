1e721fc358be35c0c591ef3d916851325faebe4e
ruanm <247987314@qq.com>
2023-10-16 11:12:06 +0800

    fix 设置全局样式(stylus css)

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx' // 表示该文件当前路径下的App.jsx（相对路径）
 import App from '@/App' // 表示src/App.jsx，等价于上面的文件地址（绝对路径）
+import '@/common/styles/frame.styl' // 全局样式
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(

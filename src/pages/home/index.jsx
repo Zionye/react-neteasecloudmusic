dec226b42d95c834237c8daf34cba23868bc9076
ruanm <247987314@qq.com>
2023-10-17 15:30:54 +0800

    fix 非Ant Design组件的主题换肤

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,20 +1,31 @@
 import { useNavigate } from 'react-router-dom'
-import { Button } from 'antd'
+import { Button, theme } from 'antd'
 import { goto } from '@/api'
 import './home.styl'
+
+const { useToken } = theme
 
 const Home = () => {
   // 创建路由钩子
   const navigate = useNavigate()
 
+  /**
+   *  获取Design Token
+   *    当前Antd文本色:       token.colorText
+   *    自定义组件的背景色换肤: token.colorBgContainer
+   *    边框色换肤:           token.colorBorder
+   *    当前Antd主题色:       token.colorPrimary
+   *  */
+  const {token: designToken} = useToken()
+
   return (
     <div className="P-home">
-      <h1>Home Page</h1>
+      <h1 style={{color: designToken.colorText}}>Home Page</h1>
       <div className="ipt-con">
           <Button type="primary" onClick={()=>{navigate('/login')}}>返回登录</Button>
       </div>
       <div className="ipt-con">
-          <Button type="primary" onClick={()=>{goto('/login')}}>组件外跳转登录</Button>
+          <Button style={{color: designToken.colorText, background: designToken.colorBgContainer}} type="primary" onClick={()=>{goto('/login')}}>组件外跳转登录</Button>
       </div>
     </div>
   )

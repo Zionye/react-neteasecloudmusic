9ca459d297efbb64b4b0f5164be59b8bb7a7d2e0
ruanm <247987314@qq.com>
2023-10-17 10:46:37 +0800

    去的console.log

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -4,6 +4,6 @@ import { globalRouters } from "@/route"
 
 // 在非React组件中进行页面路由跳转
 export const goto = (path) => {
-  console.log('globalRouters: ', globalRouters, path);
+  
   globalRouters.navigate(path)
 }

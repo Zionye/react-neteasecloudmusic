d557ecee32c7ffc5a9983d04b93b78895a270956
ruanm <247987314@qq.com>
2023-10-18 14:10:29 +0800

    fix 设置开发环境的反向代理请求

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,15 @@ export default defineConfig({
     port: 5000,
     // 自动打开浏览器运行以下页面
     open: "/",
+    // 设置反向代理(一定记得要把mock.js注释掉，否则会先被mock.js拦截，到不了反向代理这一步。) https://cn.vitejs.dev/config/server-options#server-proxy
+    proxy: {
+      // 以下示例表示：请求URL中含有"/api"，则反向代理到http://localhost
+      // 例如: http://localhost:3000/api/login -> http://localhost/api/login
+      "/api": {
+        target: "http://localhost/",
+        changeOrigin: true,
+      },
+    },
   },
   // https://cn.vitejs.dev/config/shared-options.html
   resolve: {

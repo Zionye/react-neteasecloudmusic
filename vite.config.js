9344225694aeea64a227e963cda7af587cb02c30
ruanm <247987314@qq.com>
2023-10-18 14:22:20 +0800

    fix build项目-设置build目录名称及静态资源存放目录

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    // build目录名称，默认为"dist"
+    outDir: "build",
+    // 静态资源存放目录名称，默认为"assets"
+    assetsDir: "static",
+    // // 生成map文件，默认为false（不建议设置）
+    // sourcemap: true,
+  },
   // https://cn.vitejs.dev/config/server-options.html
   server: {
     // 静态资源引用路径，默认为"/"，修改成相对路径"./"，这样在部署上线的时候不需要太关注访问目录的问题。
